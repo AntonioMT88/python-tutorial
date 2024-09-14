9980b1738069e06d78173a0afdb4d0a31e654579
Antonio Montemurro <antoniomt88@gmail.com>
2024-09-14 11:16:00 +0200

    add base_sintax code example

---
+++ base_sintax.py
@@ -0,0 +1,10 @@
+# Follow is a variable which will contain a string
+username = ""
+# Follow is a variable which will contain a number
+user_age = 0
+
+# Follow asks user to insert his name
+username = input("Hello there! Whats your name?")
+user_age = input("How old are you?")
+# Follow asks user to insert his age and print a message with all data provided
+print(f"Oooh great! So your name is {username} and you are {user_age} years old")
