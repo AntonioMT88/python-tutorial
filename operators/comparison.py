d3d6a9e8b24d45a3705aac579a4c353bf079a694
Antonio Montemurro <antoniomt88@gmail.com>
2024-10-26 11:16:47 +0200

    add comparison code example

---
+++ operators/comparison.py
@@ -0,0 +1,33 @@
+# Comparison Operator
+"""
+Equal to:                   ==
+Not equal to:               !=
+Less than:                  <
+Greater than:               >
+Less than or equal to:      <=
+Greater than or equal to:   >=
+"""
+
+a = 10
+b = 3
+
+print("Equal to")
+print(a == b)
+
+print("Not equal to")
+print(a != b)
+
+print("Less than")
+print(a < b)
+
+print("Greater than")
+print(a > b)
+
+print("Less than or equal to")
+print(a <= b)
+
+print("Greater than or equal to")
+print(a >= b)
+
+
+
