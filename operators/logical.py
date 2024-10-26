ce21ebfd84e01138d6e131152deed711a6cbbf39
Antonio Montemurro <antoniomt88@gmail.com>
2024-10-26 11:24:54 +0200

    add logical code example

---
+++ operators/logical.py
@@ -0,0 +1,20 @@
+# Logical Operator
+"""
+Logical multiplication: and
+Logical addition:       or
+Logical negation:       not
+"""
+
+a = True
+b = False
+
+print("Logical multiplication")
+print(a and b)
+
+print("Logical addition")
+print(a or b)
+
+print("Logical negation")
+print(not a)
+
+
