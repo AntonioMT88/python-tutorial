ddb15363bcf223ff9b660aa5e734146bb620ccad
Antonio Montemurro <antoniomt88@gmail.com>
2024-10-26 11:00:35 +0200

    add mathematical code example

---
+++ operators/mathematical.py
@@ -0,0 +1,34 @@
+# Mathematical Operator
+"""
+Sum             -> +
+Subtraction     -> -
+Multiplication  -> *
+Division        -> / or //
+Exponentiation  -> **
+Mod             -> %
+"""
+
+a = 10
+b = 3
+
+print("Sum Operation")
+print(a + b)
+
+print("Subtraction Operation")
+print(a - b)
+
+print("Multiplication Operation")
+print(a * b)
+
+print("Division Operation")
+print(a / b)
+print(a // b)
+
+print("Exponentiation Operation")
+print(a ** b)
+
+print("Mod Operation")
+print(a % b)
+
+
+
